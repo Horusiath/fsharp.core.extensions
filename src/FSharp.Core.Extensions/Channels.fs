4e5f96ae357c7f5b216266c91d586bc2e91f0d18
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2020-04-27 08:02:30 +0200

    Channel.select tests and fixes

--- src/FSharp.Core.Extensions/Channels.fs
+++ src/FSharp.Core.Extensions/Channels.fs
@@ -1,6 +1,25 @@
+(*
+
+Copyright 2019 Bartosz Sypytkowski
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
 namespace FSharp.Core
 
 open System
+open System.Threading
 open FSharp.Control.Tasks.Builders
 open System.Threading.Channels
 open System.Threading.Tasks
@@ -10,13 +29,38 @@ module Channel =
     /// Returns decomposed writer/reader pair components of n-capacity bounded MPSC
     /// (multi-producer/single-consumer) channel.
     let inline boundedMpsc<'a> (n: int) : (ChannelWriter<'a> * ChannelReader<'a>) =
-        let ch = Channel.CreateBounded(BoundedChannelOptions(n, SingleReader=true))
+        let ch = Channel.CreateBounded(BoundedChannelOptions(n, SingleWriter=false, SingleReader=true))
         (ch.Writer, ch.Reader)
         
     /// Returns decomposed writer/reader pair components of unbounded MPSC
     /// (multi-producer/single-consumer) channel.
     let inline unboundedMpsc<'a> () : (ChannelWriter<'a> * ChannelReader<'a>) =
-        let ch = Channel.CreateUnbounded(UnboundedChannelOptions(SingleReader=true))
+        let ch = Channel.CreateUnbounded(UnboundedChannelOptions(SingleWriter=false, SingleReader=true))
+        (ch.Writer, ch.Reader)
+       
+    
+    /// Returns decomposed writer/reader pair components of n-capacity bounded MPMC
+    /// (multi-producer/multi-consumer) channel.
+    let inline boundedMpmc<'a> (n: int) : (ChannelWriter<'a> * ChannelReader<'a>) =
+        let ch = Channel.CreateBounded(BoundedChannelOptions(n, SingleWriter=false, SingleReader=false))
+        (ch.Writer, ch.Reader)
+        
+    /// Returns decomposed writer/reader pair components of unbounded MPMC
+    /// (multi-producer/multi-consumer) channel.
+    let inline unboundedMpmc<'a> () : (ChannelWriter<'a> * ChannelReader<'a>) =
+        let ch = Channel.CreateUnbounded(UnboundedChannelOptions(SingleWriter=false, SingleReader=false))
+        (ch.Writer, ch.Reader)
+        
+    /// Returns decomposed writer/reader pair components of n-capacity bounded SPSC
+    /// (single-producer/single-consumer) channel.
+    let inline boundedSpsc<'a> (n: int) : (ChannelWriter<'a> * ChannelReader<'a>) =
+        let ch = Channel.CreateBounded(BoundedChannelOptions(n, SingleWriter=true, SingleReader=true))
+        (ch.Writer, ch.Reader)
+        
+    /// Returns decomposed writer/reader pair components of unbounded SPSC
+    /// (multi-producer/multi-consumer) channel.
+    let inline unboundedSpsc<'a> () : (ChannelWriter<'a> * ChannelReader<'a>) =
+        let ch = Channel.CreateUnbounded(UnboundedChannelOptions(SingleWriter=true, SingleReader=true))
         (ch.Writer, ch.Reader)
         
     /// Tries to read as much elements as possible from a given reader to fill provided span
@@ -28,13 +72,29 @@ module Channel =
             span.[i] <- item
             i <- i+1
         i
+         
+    let rec private awaitForValue (readers: ChannelReader<'a>[]) = vtask {
+        let pending = readers |> Array.map (fun reader -> task {
+            let! ok = reader.WaitToReadAsync()
+            if not ok then
+                return raise (ChannelClosedException("Channel has been closed"))
+            else return reader
+        })
+        let! t = Task.WhenAny(pending)
+        let ok, value = t.Result.TryRead()
+        
+        // there's a risk that another thread has read value in between WaitToReadAsync and TryRead,
+        // that's why we need to use recursion
+        if ok then return value
+        else return! awaitForValue readers
+    }
     
     /// Listens or multiple channels, returning value from the one which completed first.
     /// Order in which channels where provided will be assumed priority order in case
     /// when multiple channels have produced their values at the same time. If any of the
     /// channels will be closed while waiting or any exception will be produced, entire
     /// select will fail.
-    let select (readers: ChannelReader<'a>[]) : ValueTask<'a> = vtask {
+    let select (readers: ChannelReader<'a>[]) : ValueTask<'a> =
         // 1st pass, check in any reader has it's value already prepared
         let mutable found = false
         let mutable i = 0
@@ -44,20 +104,5 @@ module Channel =
             found <- r.TryRead(&result)
             i <- i + 1
             
-        if found then return result
-        else
-            // 2nd pass - call WaitToReadAsync and get first completing channel
-            // there's a risk that another thread has read value in between WaitToReadAsync and TryRead,
-            // that's why we need to use loop
-            let mutable finished = false
-            while not finished do
-                let pending = readers |> Array.map (fun r -> task {
-                    let! ok = r.WaitToReadAsync()
-                    if not ok then
-                        return raise (ChannelClosedException("Channel has been closed"))
-                    return r
-                })
-                let! winner = Task.WhenAny(pending)
-                finished <- winner.Result.TryRead(&result) 
-            return result
-    }
+        if found then ValueTask<'a>(result)
+        else awaitForValue readers
---
+++ tests/FSharp.Core.Extensions.Tests/ChannelTests.fs
@@ -0,0 +1,95 @@
+(*
+
+Copyright 2019 Bartosz Sypytkowski
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+module FSharp.Core.Extensions.Tests.ChannelTask
+
+open System
+open System.Threading
+open System.Threading.Tasks
+open FSharp.Core
+open FsCheck
+open Expecto
+open FSharp.Control.Tasks.Builders
+
+let private testChannel items =
+    let w, r = Channel.unboundedMpsc ()
+    for i in items do
+        w.WriteAsync(i) |> ignore
+    w, r
+    
+let inline private eval (vt: ValueTask<'a>): 'a = vt.GetAwaiter().GetResult()
+
+[<Tests>]
+let tests =
+    testList "Channel" [
+
+        testCase "readTo can return less elements than the buffer size" <| fun _ ->
+            let _, r = testChannel [| 1..3 |]                
+            let buf = Array.zeroCreate 5
+            let span = Span buf
+            let read = Channel.readTo span r
+            Expect.equal read 3 "channel had 3 elements inside"
+            Expect.equal buf [|1;2;3;0;0|] "output buffer should not be fully filled"
+            
+        testCase "readTo won't return more elements than the buffer size" <| fun _ ->
+            let _, r = testChannel [| 1..10 |]
+            let buf = Array.zeroCreate 5
+            let span = Span buf
+            let read = Channel.readTo span r
+            Expect.equal read 5 "channel had 10 elements inside, but buffer size was 5"
+            Expect.equal buf [|1;2;3;4;5|] "output buffer was fully filled"
+            
+        testCase "readTo works with empty channel" <| fun _ ->
+            let _, r = testChannel [||]
+            let buf = Array.zeroCreate 3
+            let span = Span buf
+            let read = Channel.readTo span r
+            Expect.equal read 0 "channel was empty"
+            Expect.equal buf [|0;0;0|] "nothing was written to the buffer"
+                        
+        testCase "select should not consume elements from other queues if they were not returned" <| fun _ ->
+            let _, r1 = testChannel [| 1..3 |]
+            let _, r2 = testChannel [| 11..13 |]
+            let _, r3 = testChannel [| 21..23 |]
+            let v1 = Channel.select [| r1; r2; r3 |] |> eval
+            Expect.equal v1 1 "the first item from the first channel should be returned"
+            let v2 = r2.ReadAsync() |> eval
+            Expect.equal v2 11 "second (loosing) queue should be intact after select"
+            let v3 = r3.ReadAsync() |> eval
+            Expect.equal v3 21 "third (loosing) queue should be intact after select"
+         
+        ftestCase "select should consume elements from the queue, that has received element first" <| fun _ ->
+            let w1, r1 = testChannel [||]
+            let w2, r2 = testChannel [||]
+            let selectVt = Channel.select [| r1; r2 |] // it's pending here
+            // spawn two task
+            // first will write to 1st queue after 200ms,
+            // second will write to 2nd queue after 100ms
+            let t1 = Task.run (fun () -> task {
+                do! Task.Delay(200)
+                do! w1.WriteAsync(1)
+            })
+            let t2 = Task.run (fun () -> task {
+                do! Task.Delay(100)
+                do! w2.WriteAsync(2)
+            })
+            Task.WaitAny(t1, t2) |> ignore
+            let value = selectVt |> eval
+            Expect.equal value 2 "second queue finished before first one"
+            
+    ]
