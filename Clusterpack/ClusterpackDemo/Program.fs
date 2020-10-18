8ab4ca1be48065f4cb17a08d519ea9cd2661e9fd
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2020-10-18 06:56:37 +0200

    clusterpack demo

--- Clusterpack/ClusterpackDemo/Program.fs
+++ Clusterpack/ClusterpackDemo/Program.fs
@@ -7,12 +7,6 @@ open Clusterpack.Grpc
 open FSharp.Control.Tasks.Builders
 open FSharp.Core
 
-type Greeter() =
-    inherit UnboundedActor<string>()
-    override this.Receive(msg) = unitVtask {
-        printfn "Hello %s!" msg
-    }
-
 let start nodeId endpoint =
     let transport = new GrpcTransport(nodeId, endpoint)
     new Node(transport)
@@ -21,19 +15,27 @@ let run (cancel: CancellationToken) = unitVtask {
     use a = start 1u "127.0.0.1:10001"
     use b = start 2u "127.0.0.1:10002"
     
+    // connect two nodes together - returns nodeId of node B
     let! _ = a.Connect("127.0.0.1:10002", cancel)
     
-    let local = a.Wrap(fun addr -> new Greeter())
+    // create a channel and wrap it using node A
+    let (writer, reader) = Channel.boundedMpsc 100
+    let local = a.Wrap(fun address -> writer)
+    
+    // tries to create a remote proxy using given address (address is serializable tuple (u32,u32)) 
     match b.Proxy(local.Address) with
     | None -> printfn "Couldn't target '%O' from node '%O'" local.Address b.Manifest.NodeId
     | Some remote ->
-        do! remote.WriteAsync("remote")
+        // send message to channel "living" on node A via remote proxy from node B
+        do! remote.WriteAsync("Hello from remote!")
     
-    printfn "press any key to finish... "
-    Console.Read() |> ignore
+    // receive message send remotely
+    let! message = reader.ReadAsync(cancel)
+    printfn "received %s" message
+    
+    //TODO: graceful shutdown
     do! a.DisposeAsync()
     do! b.DisposeAsync()
-    printfn "done"
 }
 
 [<EntryPoint>]
